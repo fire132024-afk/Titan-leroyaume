003e842dba54829e5f29fbd39785b5d7c01dd35f
fire132024-afk <fire13.2024@gmail.com>
2026-06-07 18:53:09 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "✔️ Besoin d’aide ? Créez un ticket !",
         // Activity type number (0 = Playing).
-        type: 5, 
+        type: 3, 
       },
     ],
   },
